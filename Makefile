d75450b5d36c4f3ccf4d6ef97e7507e2acdee02d
woldejoaquin <woldejoaquin@gmail.com>
2025-09-29 23:22:46 -0300

    Cambio de nombre de archivo de prueba

--- Makefile
+++ Makefile
@@ -139,7 +139,7 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
-	$U/_ppidtest
+	$U/_yosoytupadre
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/yosoytupadre.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+// Programa de prueba para la syscall getppid
+int
+main(void)
+{
+  int pid = getpid();
+  int ppid = getppid();
+
+  printf("Mi PID es: %d\n", pid);
+  printf("El PID de mi padre es: %d\n", ppid);
+
+  exit(0);
+}
